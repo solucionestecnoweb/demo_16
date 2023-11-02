dc5a11d0e81376569e39a69de094e9936e5d700c
marilynmillan <millan2031@gmail.com>
2023-11-02 15:28:18 -0400

    actualizar 01

--- list_contact/__manifest__.py
+++ list_contact/__manifest__.py
@@ -20,10 +20,6 @@
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
 
     'application': True,
 }
