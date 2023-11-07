55a75275cc7a8de038dd4d5ba51de95405a3689d
marilynmillan <millan2031@gmail.com>
2023-11-07 14:02:15 -0400

    actualizar carpeta

--- list_contact/__manifest__.py
+++ list_contact/__manifest__.py
@@ -19,11 +19,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/sale_document_inherit_list.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+
     ],
 
     'application': True,
